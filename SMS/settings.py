b5e4185fdf51b16affcdabb41eb42815803869c3
omyseazon <omyseazonn@gmail.com>
2022-07-15 21:51:10 +0300

    Initial commit

--- SMS/settings.py
+++ SMS/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-^h%hc10(kx-umt)3lvrdf+syj=-hxikh7j&%n#di3(l8r^&cu&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','www.acmenp.com','acmenp.com']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
